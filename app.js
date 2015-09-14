6464fe9b3407754dadc7fb020165ea684fc121a4
Jesse Gibson <overlord@Marvin.local>
2015-09-14 14:19:11 -0600

    Added new express routes, tidied code, new assets folder for scripts, styles and angular modules.

--- app.js
+++ app.js
@@ -1,56 +1,90 @@
-var express = require('express');
-var app = express();
-app.set('view engine', 'ejs');
-var path = require('path');
-
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
-
-
-var mongoose = require('mongoose');
-var db = mongoose.connection;
-mongoose.connect('mongodb://localhost/snippletdata/');
-
-db.on('error', console.error.bind(console, 'connection error:'));
-
-db.once('open',function (callback) {
-    var UserSchema = mongoose.Schema({
-        Snipplet:[]
+/*jslint nomen: true*/
+/*global require, console, __dirname*/
+(function () {
+  'use strict';
+  var express = require('express'),
+    app = express(),
+    path = require('path'),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    db = mongoose.connection,
+    server = app.listen(3000, function () {
+      var host = server.address().address,
+        port = server.address().port;
+      console.log('Example app listening at http://%s:%s', host, port);
     });
-    usersinfo = mongoose.model('Snippetinformation', UserSchema);
+
+  app.set('view engine', 'ejs');
+
+
+  app.use(bodyParser.urlencoded({
+    extended: false
+  }));
+
+
+  mongoose.connect('mongodb://localhost/snippletdata/');
+
+  db.on('error', console.error.bind(console, 'connection error:'));
+
+  db.once('open', function (callback) {
+    var UserSchema = mongoose.Schema({
+        Snipplet: []
+      }),
+      usersinfo = mongoose.model('Snippetinformation', UserSchema);
 
     app.get('/', function (req, res) {
-        usersinfo.findOne({name:'codesolutions'},function (err, snipp) {
-            console.log(snipp);
-            if (err) return console.error(err);
-            if(snipp){
-                res.render('index',{
-                    snipplets:snipp.Snipplet
-                });
-            }else{
-                res.render('index',{
-                    snipplets:[]
-                });
-            }
-        })
+      usersinfo.findOne({
+        name: 'codesolutions'
+      }, function (err, snipp) {
+        console.log(snipp || "no snippet");
+        if (err) {
+          return console.error(err);
+        }
+        if (snipp) {
+          res.render('index', {
+            snipplets: snipp.Snipplet
+          });
+        } else {
+          res.render('index', {
+            snipplets: []
+          });
+        }
+      });
+    });
+
+    app.get('/assets/styles/:sheet', function (req, res) {
+      res.sendFile(__dirname + '/assets/styles/' + req.params.sheet);
+    });
+
+    app.get('/assets/scripts/:script', function (req, res) {
+      res.sendFile(__dirname + '/assets/scripts/' + req.params.script);
+    });
+
+    app.get('/assets/directives/:module', function (req, res) {
+      res.sendFile(__dirname + '/assets/directives/' + req.params.module);
     });
 
     app.get('/codeadded', function (req, res) {
-        res.render('codeadded');
+      res.render('codeadded');
     });
 
-    app.post('/codeenterd',function(req,res){
-        usersinfo.update({'name':'codesolutions'}, {$set:{Snipplet:req.body.added}}, {upsert: true}, function (err) {
-            if (err) return console.error(err);
-            else
-                res.redirect('/')
-        });
+    app.post('/codeentered', function (req, res) {
+      usersinfo.update({
+        'name': 'codesolutions'
+      }, {
+        $set: {
+          Snipplet: req.body.added
+        }
+      }, {
+        upsert: true
+      }, function (err) {
+        if (err) {
+          return console.error(err);
+        } else {
+          res.redirect('/');
+        }
+      });
     });
 
-});//ending of data base on connect
-
-var server = app.listen(3000, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log('Example app listening at http://%s:%s', host, port);
-});
+  }); //ending of data base on connect
+}());
