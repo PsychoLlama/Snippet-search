b8184875699dd2590b6b4db2bf6676b3e58b7293
alex <alexgwartney@aol.com>
2015-09-14 01:39:23 -0700

    Added in data base and data base functonality to get users infomration and store on data base

--- app.js
+++ app.js
@@ -3,13 +3,51 @@ var app = express();
 app.set('view engine', 'ejs');
 var path = require('path');
 
-app.get('/', function(req,res){
-    res.render('index')
-});
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/codeadded', function (req, res) {
-    res.render('codeadded');
-});
+
+var mongoose = require('mongoose');
+var db = mongoose.connection;
+mongoose.connect('mongodb://localhost/snippletdata/');
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open',function (callback) {
+    var UserSchema = mongoose.Schema({
+        Snipplet:[]
+    });
+    usersinfo = mongoose.model('Snippetinformation', UserSchema);
+
+    app.get('/', function (req, res) {
+        usersinfo.findOne({name:'codesolutions'},function (err, snipp) {
+            console.log(snipp);
+            if (err) return console.error(err);
+            if(snipp){
+                res.render('index',{
+                    snipplets:snipp.Snipplet
+                });
+            }else{
+                res.render('index',{
+                    snipplets:[]
+                });
+            }
+        })
+    });
+
+    app.get('/codeadded', function (req, res) {
+        res.render('codeadded');
+    });
+
+    app.post('/codeenterd',function(req,res){
+        usersinfo.update({'name':'codesolutions'}, {$set:{Snipplet:req.body.added}}, {upsert: true}, function (err) {
+            if (err) return console.error(err);
+            else
+                res.redirect('/')
+        });
+    });
+
+});//ending of data base on connect
 
 var server = app.listen(3000, function () {
     var host = server.address().address;
