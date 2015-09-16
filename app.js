f98261cd5595075f1d6274c2dddedc2c9910a67b
alex <alexgwartney@aol.com>
2015-09-15 23:56:59 -0700

    moved search box to index page and added in the ejs keys

--- app.js
+++ app.js
@@ -19,28 +19,42 @@ db.once('open',function (callback) {
     });
     usersinfo = mongoose.model('Snippetinformation', UserSchema);
 
-    app.get('/', function (req, res) {
-        usersinfo.findOne({name:'codesolutions'},function (err, snipp) {
-            console.log(snipp);
-            if (err) return console.error(err);
-            if(snipp){
-                res.render('index',{
-                    snipplets:snipp.Snipplet
-                });
-            }else{
-                res.render('index',{
-                    snipplets:[]
-                });
-            }
-        })
-    });
+
 
     app.get('/codeadded', function (req, res) {
         res.render('codeadded');
     });
 
+    app.get('/',function(req,res){
+       res.render('index',{
+           snipplets:''
+       })
+    });
+    app.post('/search',function(req,res){
+        var title = req.body.search;
+        console.log(req.body);
+        usersinfo.findOne({Snipplet:{$elemMatch:{title:new RegExp(title, "i")}}},{Snipplet:{$elemMatch:{title:new RegExp(title, "i")}}},function (err, snipp) {
+            console.log(snipp);
+            if (err) return console.error(err);
+            if(snipp){
+                res.render('index',{
+                    snipplets:snipp.Snipplet[0]
+                });
+            }else{
+                res.render('index',{
+                    snipplets:{}
+                });
+            }
+        })
+    });
+
     app.post('/codeenterd',function(req,res){
-        usersinfo.update({'name':'codesolutions'}, {$set:{Snipplet:req.body.added}}, {upsert: true}, function (err) {
+        var updateobject = {
+            title:req.body.title,
+            body:req.body.codesnippet
+        };
+
+        usersinfo.update({'name':'codesolutions'},{$push:{Snipplet:updateobject}}, {upsert: true}, function (err) {
             if (err) return console.error(err);
             else
                 res.redirect('/')
@@ -54,3 +68,7 @@ var server = app.listen(3000, function () {
     var port = server.address().port;
     console.log('Example app listening at http://%s:%s', host, port);
 });
+
+
+
+
