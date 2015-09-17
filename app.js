3029c6020010d788513cf3edc8663fe4b64c9ccc
Jesse Gibson <overlord@Marvin.local>
2015-09-17 15:54:13 -0600

    Now using the Ace editor instead of textarea, satisfied JSLint, fixed dependency serving by routing to the assets folder and some other minor changes.

--- app.js
+++ app.js
@@ -1,86 +1,127 @@
-var express = require('express');
-var app = express();
-app.set('view engine', 'ejs');
-var path = require('path');
+/*globals require, console */
+(function () {
+  'use strict';
 
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
+  var bodyParser = require('body-parser'),
+    path = require('path'),
+    mongoose = require('mongoose'),
+    db = mongoose.connection,
+    express = require('express'),
+    app = express(),
+    server;
 
-app.use(express.static(path.join(__dirname, 'javascript')));
+  app.set('view engine', 'ejs');
 
-var mongoose = require('mongoose');
-var db = mongoose.connection;
-mongoose.connect('mongodb://localhost/snippletdata/');
+  app.use(bodyParser.urlencoded({
+    extended: false
+  }));
 
-db.on('error', console.error.bind(console, 'connection error:'));
+  app.use('/assets', express['static']('assets'));
 
-db.once('open',function (callback) {
+  mongoose.connect('mongodb://localhost/snippletdata/');
+
+  db.on('error', console.error.bind(console, 'connection error:'));
+
+  db.once('open', function (callback) {
     var UserSchema = mongoose.Schema({
-        Snipplet:[]
-    });
-    usersinfo = mongoose.model('Snippetinformation', UserSchema);
+        Snipplet: []
+      }),
+      usersinfo = mongoose.model('Snippetinformation', UserSchema);
 
 
 
     app.get('/codeadded', function (req, res) {
-        res.render('codeadded');
+      res.render('codeadded');
     });
 
-    app.get('/',function(req,res){
-       res.render('index',{
-           snipplets:''
-       })
+    app.get('/', function (req, res) {
+      res.render('index', {
+        snipplets: ''
+      });
     });
-    app.post('/search',function(req,res){
-        var title = req.body.search;
-        console.log(req.body);
-        usersinfo.findOne({Snipplet:{$elemMatch:{title:new RegExp(title, "i")}}},{Snipplet:{$elemMatch:{title:new RegExp(title, "i")}}},function (err, snipp) {
-            console.log(snipp);
-            if (err) return console.error(err);
-            if(snipp){
-                res.render('index',{
-                    snipplets:snipp.Snipplet[0]
-                });
-            }else{
-                res.render('index',{
-                    snipplets:{}
-                });
-            }
-        })
-    });
-
-    app.post('/codeenterd',function(req,res){
-        var updateobject = {
-            title:req.body.title,
-            body:req.body.codesnippet,
-            desc:req.body.discription
-        };
-
-        usersinfo.update({'name':'codesolutions'},{$push:{Snipplet:updateobject}}, {upsert: true}, function (err) {
-            if (err) return console.error(err);
-            else
-                res.redirect('/')
-        });
+    app.post('/search', function (req, res) {
+      var title = req.body.search;
+      usersinfo.findOne({
+        Snipplet: {
+          $elemMatch: {
+            title: new RegExp(title, "i")
+          }
+        }
+      }, {
+        Snipplet: {
+          $elemMatch: {
+            title: new RegExp(title, "i")
+          }
+        }
+      }, function (err, snipp) {
+        if (err) {
+          return console.error(err);
+        }
+        if (snipp) {
+          res.render('index', {
+            snipplets: snipp.Snipplet[0]
+          });
+        } else {
+          res.render('index', {
+            snipplets: {}
+          });
+        }
+      });
     });
 
+    app.post('/codeenterd', function (req, res) {
+      var updateobject = {
+        title: req.body.title,
+        body: req.body.codesnippet,
+        desc: req.body.description
+      };
 
-    app.post('/delete', function(req, res){
-        console.log(req.body);
-        usersinfo.findOneAndUpdate({'name':'codesolutions'},{$pull:{Snipplet:{body:req.body.filetodelete}}},function(err) {
-            if (err) return console.error(err);
-            else
-                res.redirect('/');
-        });
+      usersinfo.update({
+        'name': 'codesolutions'
+      }, {
+        $push: {
+          Snipplet: updateobject
+        }
+      }, {
+        upsert: true
+      }, function (err) {
+        if (err) {
+          return console.error(err);
+        } else {
+          res.redirect('/');
+        }
+      });
+    });
+
+
+    app.post('/delete', function (req, res) {
+      console.log(req.body);
+      usersinfo.findOneAndUpdate({
+        'name': 'codesolutions'
+      }, {
+        $pull: {
+          Snipplet: {
+            body: req.body.filetodelete
+          }
+        }
+      }, function (err) {
+        if (err) {
+          return console.error(err);
+        } else {
+          res.redirect('/');
+        }
+      });
+
     });
 
 
 
-});//ending of data base on connect
+  }); // ending of data base on connect
 
-var server = app.listen(3000, function () {
-    var host = server.address().address;
-    var port = server.address().port;
+  server = app.listen(3000, function () {
+    var host = server.address().address,
+      port = server.address().port;
     console.log('Example app listening at http://%s:%s', host, port);
-});
+  });
 
-
+}());
---
+++ assets/scripts/validation.js
@@ -0,0 +1,30 @@
+/*globals hljs*/
+(function () {
+  'use strict';
+
+  var titles = document.querySelectorAll('.thumbnail h3'),
+    descriptions = document.querySelectorAll('.thumbnail p'),
+    codeBlocks = document.getElementsByTagName('code'),
+    placeholder = "window.onload = function () {\n  console.log('Hello world!');\n}",
+    setDefault,
+    i;
+
+
+  // Set default messages for the code snippet fields
+  setDefault = function (nodeList, message) {
+
+    for (i = 0; i < nodeList.length; i += 1) {
+      var node = nodeList[i];
+
+      if (node.innerHTML.match(/^\s*$/g)) {
+        node.innerHTML = message;
+      }
+    }
+  };
+
+  setDefault(titles, 'No title');
+  setDefault(descriptions, 'No description');
+  setDefault(codeBlocks, placeholder);
+
+  hljs.initHighlightingOnLoad();
+}());
