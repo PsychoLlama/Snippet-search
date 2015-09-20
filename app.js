ea6dd77a73ae840a0086552591e7c5ea3a77dc73
alex <alexgwartney@aol.com>
2015-09-19 22:39:12 -0700

    Cleaned up code

--- app.js
+++ app.js
@@ -30,89 +30,11 @@
       }),
       usersinfo = mongoose.model('snippletdata2', UserSchema);
 
-
-
     app.get('/codeadded',routes.codeadded);
-
-    app.get('/', function (req, res) {
-      res.render('index', {
-        snipplets: ''
-      });
-    });
-    app.post('/search', function (req, res) {
-      var title = req.body.search;
-      usersinfo.findOne({
-        Snipplet: {
-          $elemMatch: {
-            title: new RegExp(title, "i")
-          }
-        }
-      }, {
-        Snipplet: {
-          $elemMatch: {
-            title: new RegExp(title, "i")
-          }
-        }
-      }, function (err, snipp) {
-        if (err) {
-          return console.error(err);
-        }
-        if (snipp) {
-          res.render('index', {
-            snipplets: snipp.Snipplet[0]
-          });
-        } else {
-          res.render('index', {
-            snipplets: null
-          });
-        }
-      });
-    });
-
-    app.post('/codeenterd', function (req, res) {
-      var updateobject = {
-        title: req.body.title,
-        body: req.body.codesnippet,
-        desc: req.body.description
-      };
-
-      usersinfo.update({
-        'name': 'codesolutions'
-      }, {
-        $push: {
-          Snipplet: updateobject
-        }
-      }, {
-        upsert: true
-      }, function (err) {
-        if (err) {
-          return console.error(err);
-        } else {
-          res.redirect('/');
-        }
-      });
-    });
-
-
-    app.post('/delete', function (req, res) {
-      console.log(req.body);
-      usersinfo.findOneAndUpdate({
-        'name': 'codesolutions'
-      }, {
-        $pull: {
-          Snipplet: {
-            body: req.body.filetodelete
-          }
-        }
-      }, function (err) {
-        if (err) {
-          return console.error(err);
-        } else {
-          res.redirect('/');
-        }
-      });
-
-    });
+    app.get('/',routes.index);
+    app.post('/search',routes.search);
+    app.post('/codeenterd',routes.codeenterd);
+    app.post('/delete',routes.delete);
 
   }); // ending of data base on connect
 var Port  = process.env["PORT"] || 8080;
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,83 @@
 exports.codeadded = function (req, res) {
     res.render('codeadded');
 };
+
+exports.index  = function (req, res) {
+    res.render('index', {
+        snipplets: ''
+    });
+};
+
+exports.search = function (req, res) {
+    var title = req.body.search;
+    usersinfo.findOne({
+        Snipplet: {
+            $elemMatch: {
+                title: new RegExp(title, "i")
+            }
+        }
+    }, {
+        Snipplet: {
+            $elemMatch: {
+                title: new RegExp(title, "i")
+            }
+        }
+    }, function (err, snipp) {
+        if (err) {
+            return console.error(err);
+        }
+        if (snipp) {
+            res.render('index', {
+                snipplets: snipp.Snipplet[0]
+            });
+        } else {
+            res.render('index', {
+                snipplets: null
+            });
+        }
+    });
+};
+
+exports.codeenterd = function (req, res) {
+    var updateobject = {
+        title: req.body.title,
+        body: req.body.codesnippet,
+        desc: req.body.description
+    };
+
+    usersinfo.update({
+        'name': 'codesolutions'
+    }, {
+        $push: {
+            Snipplet: updateobject
+        }
+    }, {
+        upsert: true
+    }, function (err) {
+        if (err) {
+            return console.error(err);
+        } else {
+            res.redirect('/');
+        }
+    });
+};
+
+exports.delete =  function (req, res) {
+    console.log(req.body);
+    usersinfo.findOneAndUpdate({
+        'name': 'codesolutions'
+    }, {
+        $pull: {
+            Snipplet: {
+                body: req.body.filetodelete
+            }
+        }
+    }, function (err) {
+        if (err) {
+            return console.error(err);
+        } else {
+            res.redirect('/');
+        }
+    });
+
+};
