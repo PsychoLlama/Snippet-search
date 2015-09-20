355808d8959c0a99bcb30ce898e1de70e5774e47
alex <alexgwartney@aol.com>
2015-09-19 20:46:33 -0700

    Cleaned up code

--- app.js
+++ app.js
@@ -5,10 +5,10 @@
   var bodyParser = require('body-parser'),
     path = require('path'),
       module = require('./databaseinfo'),
+      routes = require('.routes'),
     mongoose = require('mongoose'),
     db = mongoose.connection,
     express = require('express'),
-      routes = require('./routes'),
     app = express(),
     server;
 
@@ -25,13 +25,96 @@
   db.on('error', console.error.bind(console, 'connection error:'));
 
   db.once('open', function (callback) {
-    app.get('/codeadded',routes.codeadd);
-    app.get('/',routes.index);
-    app.post('/search',routes.search);
-    app.post('/codeenterd',routes.codeadded);
-    app.post('/delete',routes.delete);
-  }); // ending of data base on connect
+    var UserSchema = mongoose.Schema({
+        Snipplet: []
+      }),
+      usersinfo = mongoose.model('snippletdata2', UserSchema);
 
+
+
+    app.get('/codeadded',routes.codeadded);
+
+    app.get('/', function (req, res) {
+      res.render('index', {
+        snipplets: ''
+      });
+    });
+    app.post('/search', function (req, res) {
+      var title = req.body.search;
+      usersinfo.findOne({
+        Snipplet: {
+          $elemMatch: {
+            title: new RegExp(title, "i")
+          }
+        }
+      }, {
+        Snipplet: {
+          $elemMatch: {
+            title: new RegExp(title, "i")
+          }
+        }
+      }, function (err, snipp) {
+        if (err) {
+          return console.error(err);
+        }
+        if (snipp) {
+          res.render('index', {
+            snipplets: snipp.Snipplet[0]
+          });
+        } else {
+          res.render('index', {
+            snipplets: null
+          });
+        }
+      });
+    });
+
+    app.post('/codeenterd', function (req, res) {
+      var updateobject = {
+        title: req.body.title,
+        body: req.body.codesnippet,
+        desc: req.body.description
+      };
+
+      usersinfo.update({
+        'name': 'codesolutions'
+      }, {
+        $push: {
+          Snipplet: updateobject
+        }
+      }, {
+        upsert: true
+      }, function (err) {
+        if (err) {
+          return console.error(err);
+        } else {
+          res.redirect('/');
+        }
+      });
+    });
+
+
+    app.post('/delete', function (req, res) {
+      console.log(req.body);
+      usersinfo.findOneAndUpdate({
+        'name': 'codesolutions'
+      }, {
+        $pull: {
+          Snipplet: {
+            body: req.body.filetodelete
+          }
+        }
+      }, function (err) {
+        if (err) {
+          return console.error(err);
+        } else {
+          res.redirect('/');
+        }
+      });
+
+    });
+
+  }); // ending of data base on connect
 var Port  = process.env["PORT"] || 8080;
 app.listen(Port);
 
---
+++ routes/index.js
@@ -0,0 +1,3 @@
+exports.codeadded = function (req, res) {
+    res.render('codeadded');
+};
