31772d3c96e55902fbf77a87ce4e8a90821c931e
Jesse Gibson <overlord@Marvin.local>
2015-09-25 14:52:27 -0600

    Remove procfile and databaseinfo.

--- app.js
+++ app.js
@@ -1,43 +1,40 @@
 /*globals require, console */
+/*jslint node: true*/
+'use strict';
 
-  'use strict';
+var bodyParser = require('body-parser'),
+  routes = require('./routes/index'),
+  mongoose = require('mongoose'),
+  db = mongoose.connection,
+  express = require('express'),
+  app = express(),
+  Port = process.env.PORT || 8080;
 
-  var bodyParser = require('body-parser'),
-    path = require('path'),
-      module = require('./databaseinfo'),
-      routes = require  ('./routes/index'),
-    mongoose = require('mongoose'),
-    db = mongoose.connection,
-    express = require('express'),
-    app = express();
+app.set('view engine', 'ejs');
 
+app.use(express['static']('public'));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
-  app.set('view engine', 'ejs');
+mongoose.connect('mongodb://localhost/snippetdata/');
 
-  app.use(bodyParser.urlencoded({
-    extended: false
-  }));
+db.on('error', console.error.bind(console, 'Connection error:'));
 
-  app.use('/assets', express['static']('assets'));
+db.once('open', function () {
+  // Routes
+  app.get('/codeadded', routes.codeadded);
+  app.get('/', routes.index);
+  app.post('/search', routes.search);
+  app.post('/codeentered', routes.codeentered);
+  app.post('/delete', routes["delete"]);
+});
 
-  mongoose.connect(module);
-
-  db.on('error', console.error.bind(console, 'connection error:'));
-
-  db.once('open', function (callback) {
-
-
-    app.get('/codeadded',routes.codeadded);
-    app.get('/',routes.index);
-    app.post('/search',routes.search);
-    app.post('/codeenterd',routes.codeenterd);
-    app.post('/delete',routes.delete);
-
-  }); // ending of data base on connect
-var Port  = process.env["PORT"] || 8080;
-app.listen(Port);
+app.listen(Port, function () {
+  console.log("listening on port", Port);
+});
 
 process.on('uncaughtException', function (err) {
-  console.log("\n\r Uncaught Exception event \n\r");
+  console.log("\nUncaught Exception event\n\n");
   console.log(err);
 });
